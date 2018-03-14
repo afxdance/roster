395bd3856123dbdeb9c70417a8fd4d85705bb5c0
Sean Zhu <interestinglythere@gmail.com>
2018-03-14 02:58:54 -0700

    Add initial models

---
+++ app/models/team_switch_request.rb
@@ -0,0 +1,6 @@
+class TeamSwitchRequest < ApplicationRecord
+  belongs_to :dancer
+  has_and_belongs_to_many :available_teams, class_name: 'Team', join_table: :team_switch_requests_available_teams
+  belongs_to :old_team, class_name: 'Team'
+  belongs_to :new_team, class_name: 'Team'
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180311003653) do
+ActiveRecord::Schema.define(version: 20180314023526) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -24,6 +24,63 @@ ActiveRecord::Schema.define(version: 20180311003653) do
     t.index ["author_type", "author_id"], name: "index_active_admin_comments_on_author_type_and_author_id"
     t.index ["namespace"], name: "index_active_admin_comments_on_namespace"
     t.index ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource_type_and_resource_id"
+  end
+
+  create_table "dancers", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "phone"
+    t.string "gender"
+    t.string "year"
+    t.string "experience"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "dancers_teams", id: false, force: :cascade do |t|
+    t.integer "dancer_id", null: false
+    t.integer "team_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["dancer_id", "team_id"], name: "index_dancers_teams_on_dancer_id_and_team_id"
+    t.index ["team_id", "dancer_id"], name: "index_dancers_teams_on_team_id_and_dancer_id"
+  end
+
+  create_table "team_switch_requests", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "phone"
+    t.text "reason"
+    t.datetime "approved_at"
+    t.string "status"
+    t.integer "old_team_id"
+    t.integer "new_team_id"
+    t.integer "dancer_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["dancer_id"], name: "index_team_switch_requests_on_dancer_id"
+    t.index ["new_team_id"], name: "index_team_switch_requests_on_new_team_id"
+    t.index ["old_team_id"], name: "index_team_switch_requests_on_old_team_id"
+  end
+
+  create_table "team_switch_requests_available_teams", id: false, force: :cascade do |t|
+    t.integer "team_switch_request_id", null: false
+    t.integer "team_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["team_id", "team_switch_request_id"], name: "index_team_switch_requests_available_teams_on_team_and_request"
+    t.index ["team_switch_request_id", "team_id"], name: "index_team_switch_requests_available_teams_on_request_and_team"
+  end
+
+  create_table "teams", force: :cascade do |t|
+    t.string "type"
+    t.string "name"
+    t.string "practice_time"
+    t.string "locked"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_teams_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
