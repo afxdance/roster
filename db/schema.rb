cb5f20581ecb62b637fe89e33ef1d6860cbbdd7d
Sean Zhu <interestinglythere@gmail.com>
2018-03-20 04:29:26 -0700

    Rubocop auto-correct

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 20180314023526) do
-
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
     t.text "body"
@@ -99,5 +98,4 @@ ActiveRecord::Schema.define(version: 20180314023526) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
-
 end
