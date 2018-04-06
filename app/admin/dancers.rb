d5eee36e8fa23c7f01061f32084b6341bea44448
Alice Wu <alwu5269@gmail.com>
2018-04-05 22:45:55 -0700

    Created basic skeleton

--- app/admin/dancers.rb
+++ app/admin/dancers.rb
@@ -17,9 +17,73 @@ ActiveAdmin.register Dancer do
       :name,
       :email,
       :phone,
-      :reason,
-      :approved_at,
-      :status,
+      :gender,
+      # :reason,
+      # :approved_at,
+      # :status,
     ].compact
+  end
+
+  member_action :add_to_team, method: :post do
+    ids = params[:id]
+    add_helper(ids)
+  end
+
+  member_action :remove_from_team, method: :post do
+    ids = params[:id]
+    remove_helper(ids)
+  end
+
+  controller do
+    def add_helper(ids)
+      redirect_to '/admin/dancers', :alert => "to add dancers at index #{ids}"
+    end
+
+    def remove_helper(ids)
+      redirect_to '/admin/dancers', :alert => "to remove dancers at index #{ids}"
+    end
+  end
+
+  form do |f|
+    f.inputs do
+      f.input :name
+      f.input :email
+      f.input :phone
+      # f.input :reason
+      # f.input :approved_at
+      # f.input :status
+    end
+    f.actions
+  end
+
+  index do
+    selectable_column
+    column :id
+    column :name
+    column :email
+    column :phone
+    # column :reason
+    # column :approved_at
+    # column :status
+    column :add_dancer do |dancer|
+      link_to "Add", "/admin/dancers/#{dancer.id}/add_to_team" , method: :post
+    end
+    column :remove_dancer do |dancer|
+      link_to "Remove", "/admin/dancers/#{dancer.id}/remove_from_team" , method: :post
+    end
+    actions
+  end
+
+  show do |dancers|
+    panel "Detail" do
+      attributes_table_for dancers do
+        row :name
+        row :email
+        row :phone
+        row :reason
+        row :approved_at
+        row :status
+      end
+    end
   end
 end
--- app/admin/teams.rb
+++ app/admin/teams.rb
@@ -17,6 +17,36 @@ ActiveAdmin.register Team do
       :name,
       :practice_time,
       :locked,
+      :project,
     ].compact
+  end
+
+  form do |f|
+    f.inputs do
+      f.input :name
+      f.input :practice_time
+      f.input :locked
+    end
+    f.actions
+  end
+
+  index do
+    selectable_column
+    column :name
+    column :practice_time
+    column :locked
+    column :project
+    actions
+  end
+
+  show do
+    panel "Detail" do
+      attributes_table_for team do
+        row :name
+        row :practice_time
+        row :locked
+        row :project
+      end
+    end
   end
 end
