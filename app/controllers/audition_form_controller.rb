2175e3eb9485930f508661a49207a316063b0e06
Sheldon C <default_user@MacBook-Pro-6.local>
2018-04-08 12:49:50 -0700

    all fields required, start working on edit functionality

--- app/controllers/audition_form_controller.rb
+++ app/controllers/audition_form_controller.rb
@@ -13,6 +13,31 @@ class AuditionFormController < ApplicationController
     end
   end
 
+  def edit_dancer
+    @dancer = Dancer.find(dancer_params[:id])
+    if @dancer.update_attributes(dancer_params[:post])
+      render 'audition_form/number'
+    else
+      render 'edit'
+    end
+  end
+  #   respond_to do |format|
+  #     if @dancer.update_attributes(params[:post])
+  #       format.html  { redirect_to(@dancer,
+  #                     :notice => 'Post was successfully updated.') }
+  #       format.json  { head :no_content }
+  #     else
+  #       format.html  { render :action => "edit" }
+  #       format.json  { render :json => @dancer.errors,
+  #                     :status => :unprocessable_entity }
+  #     end
+  #   end
+  # end
+
+  def edit
+    @dancer = Dancer.find(dancer_params[:id])
+  end
+
   private
 
   def dancer_params
