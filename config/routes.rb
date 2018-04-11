43ab8490ed824bcc105f41916be8a7505e8c182f
Tanvee <tanvee@thebest360.localdomain>
2018-04-10 21:33:40 -0700

    added team switch to routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   devise_for :users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/teamswitchform', to: 'team_switch_form#index'
+  post '/teamswitchform', to: 'team_switch_form#create_team_switch_request', as: "team_switch_form"
 end
