6f766ea3421bbc01218bba81bed890deda4ccbdd
Vincent Tantra <vincentxtantra@gmail.com>
2018-04-14 19:26:47 -0700

    rubocop --auto-correct

--- app/controllers/audition_form_controller.rb
+++ app/controllers/audition_form_controller.rb
@@ -1,15 +1,15 @@
 class AuditionFormController < ApplicationController
   def index
     @dancer = Dancer.new
-    render 'audition_form/form'
+    render "audition_form/form"
   end
 
   def create_dancer
     @dancer = Dancer.new(dancer_params)
     if @dancer.save
-      render 'audition_form/number'
+      render "audition_form/number"
     else
-      render 'audition_form/form'
+      render "audition_form/form"
     end
   end
 
@@ -17,23 +17,21 @@ class AuditionFormController < ApplicationController
     @dancer = Dancer.find(params[:id])
   end
 
-  def update
-  end
-
+  def update; end
 
   private
 
-  def dancer_params
-    params.require(:dancer).permit(
-      :devinterest,
-      :email,
-      :experience,
-      :expinterest,
-      :gender,
-      :name,
-      :phone,
-      :year,
-      # availability: params[:dancer][:availability] ? params[:dancer][:availability].keys : nil
-    )
-  end
+    def dancer_params
+      params.require(:dancer).permit(
+        :devinterest,
+        :email,
+        :experience,
+        :expinterest,
+        :gender,
+        :name,
+        :phone,
+        :year,
+        # availability: params[:dancer][:availability] ? params[:dancer][:availability].keys : nil
+      )
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # audition_form_controller
-  get '/auditionform', to: 'audition_form#index'
-  post '/auditionform', to: 'audition_form#create_dancer', as: "audition_form"
+  get "/auditionform", to: "audition_form#index"
+  post "/auditionform", to: "audition_form#create_dancer", as: "audition_form"
 
   # edit_form_controller
-  get '/auditionform/:id/edit', to: 'audition_form#edit', as: :edit_dancer
-  put '/auditionform/:id', to: 'audition_form#update'
-
+  get "/auditionform/:id/edit", to: "audition_form#edit", as: :edit_dancer
+  put "/auditionform/:id", to: "audition_form#update"
 end
