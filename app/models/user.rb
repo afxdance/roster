91bdbe44dba115c817f91ccbae67965314c3e3cb
Alice Wu <alwu5269@gmail.com>
2018-04-19 21:53:51 -0700

    essentially finished adding and removing functionality but like teams are associated with users and life is hard and peter is dumb and stella doesnt know what anything is

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  belongs_to :team
+  # belongs_to :team
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable,
