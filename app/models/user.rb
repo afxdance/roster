afb9c33fd0b57c35ecf5e674e95e2ac8b48e4f75
Sean Zhu <interestinglythere@gmail.com>
2018-06-25 07:06:17 -0700

    Use username logins instead of emails! (#19)

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,24 @@ class User < ApplicationRecord
   has_and_belongs_to_many :teams
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise(
+    :database_authenticatable,
+    :recoverable,
+    :rememberable,
+    :trackable,
+    :validatable,
+    authentication_keys: [:username],
+  )
+
+  def email_required?
+    false
+  end
+
+  def email_changed?
+    false
+  end
+
+  def will_save_change_to_email?
+    false
+  end
 end
