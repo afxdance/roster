5d22238efad5196f604674f8a121ee0880d927c3
Sean Zhu <interestinglythere@gmail.com>
2018-09-27 23:00:59 -0700

    Updates for Fall 2018 (#24)
    * Hide gender column for non-admins
    
    * Hide comments tab
    
    * Set audition number + csv import [Stella/Sean]
    
    * Pass through error messages into flash message
    
    * Rubocop
    
    * Make CircleCI work
    
    * Changed list of dancers location and view [Stella]
    
    * deleted test stuff
    
    * rubocop gem file
    
    * added join separators
    
    * Simple permissions checking
    
    * More details in team show view
    
    * Allow changing entries per page
    
    * Make dancer table layout more bearable
    
    * Randomization
    
    * Fix for heroku
    
    * Team switch form
    
    * No pry pls
    
    * Show the right thing
    
    * lolwhoops
    
    * changed text
    
    * Actually save which teams the dancers are available for
    
    * Complete team switch form
    
    * Randomize only onto training teams
    
    * Show added reason and date
    
    * time zone
    
    * Allow multiple ways to sort dancers on a team
    
    * Allow team switches
    
    * typo
    
    * Allow fully editing of team switch requests
    
    * Remove busy/free indicator from team switch form
    
    * Add checkbox validation to team switch form
    
    * Team switch requests: show team balance
    
    * Allow filtering for empty statuses
    
    * Allow hiding teams from the team switch form
    
    * Allow directors to drop people from teams
    
    * Close team switch form
    
    * updates to audition form for fall 18
    
    * change which interest fields are displayed
    
    * removed edit link from audition form, removed edit routes
    
    * Updated switch/drop form for Fa18
    
    * Updated switch/drop form for Fa18
    
    * Updated switch/drop form for Fa18
    
    * Updated switch/drop form for Fa18
    
    * Fixed wording
    
    * Fix rubocop
    
    * Fix rubocop
    
    (cherry picked from commit 9c98186dae2231dfff82439a81cff8d69edeab44)

--- app/models/dancer.rb
+++ app/models/dancer.rb
@@ -34,9 +34,9 @@ class Dancer < ApplicationRecord
     "false" => "No",
   }.freeze
 
-  SHOW_CAMP_INTEREST = true
-  SHOW_EXP_INTEREST = false
-  SHOW_TECH_INTEREST = false
+  SHOW_CAMP_INTEREST = false
+  SHOW_EXP_INTEREST = true
+  SHOW_TECH_INTEREST = true
 
   REQUIRED_FIELDS = [
     :name,
