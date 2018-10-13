2697e4f3c6dcaa7f22a2772d41ef8471df26eac9
patriciayu7 <patriciayu7@berkeley.edu>
2018-10-13 13:20:59 -0700

    started whitespace team switc request test

--- test/models/team_switch_request_test.rb
+++ test/models/team_switch_request_test.rb
@@ -1,7 +1,27 @@
 require "test_helper"
 
-class TeamSwitchRequestTest < ActiveSupport::TestCase
+class TeamSwitchRequestTest < Capybara::Rails::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  include ActiveJob::TestHelper
+
+  def setup
+    @one = dancers :one
+    @two = dancers :two
+    @project = teams :project
+    @training = teams :training
+    @onerequest = onerequest :team_switch_requests
+    @tworequest = tworequest :team_switch_requests
+  end
+
+  test 'whitespace in name' do
+    process_team_switch_request_into_team(team_switch_request_id, team_id)
+    visit teamswitch_path
+
+    assert page.has_content?(@one.title)
+    assert page.has_content?(@two.title)
+  end
+
 end
