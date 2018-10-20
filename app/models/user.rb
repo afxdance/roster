5ffe2a7fb10aeaff9e87276b405405c43b14e061
michellefan <fan.michellec@gmail.com>
2018-10-20 12:32:34 -0700

    created user role column

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_and_belongs_to_many :teams
+  enum role: [:director, :admin]
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise(
