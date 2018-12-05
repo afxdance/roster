b6ffa73f82e56a3c20ca3e1b72952dfb29935742
patriciayu7 <patriciayu7@berkeley.edu>
2018-12-05 14:06:35 -0800

    add question mark to users

--- db/schema.rb
+++ db/schema.rb
@@ -113,7 +113,7 @@ ActiveRecord::Schema.define(version: 20181103192805) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "role"
+    t.integer "role", null: false
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
   end
