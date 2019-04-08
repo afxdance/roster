f977ad96daacd10c4c985db025786bb94b0cc013
Cynthia Wu <ckwu@berkeley.edu>
2019-04-07 21:01:43 -0700

    call dancer emails using pluck

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,6 +2,6 @@ class UserMailer < ApplicationMailer
   default from: 'afxroster@gmail.com'
 
   def example_email
-    mail(bcc: ['ckwu@berkeley.edu', 'jessie.jin@berkeley.edu', 'youngcai@berkeley.edu'], subject: 'test')
+    mail(bcc: Dancer.pluck(:email), subject: 'test')
   end
 end
