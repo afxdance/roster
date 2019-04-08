a0aa3e470a904a22e8d1c5ff8fd1cc158fe8ddef
Cynthia Wu <ckwu@berkeley.edu>
2019-04-07 21:58:21 -0700

    complete audition email, dance exp bug remaining

--- app/controllers/audition_controller.rb
+++ app/controllers/audition_controller.rb
@@ -6,7 +6,7 @@ class AuditionController < ApplicationController
   def create
     @dancer = Dancer.new(dancer_params)
     success = @dancer.save
-    UserMailer.example_email.deliver_now
+    UserMailer.audition_email(@dancer).deliver_now
     render "audition/index" unless success
   end
 
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,4 +4,10 @@ class UserMailer < ApplicationMailer
   def example_email
     mail(bcc: Dancer.pluck(:email), subject: 'test')
   end
+
+  def audition_email(user)
+    @user = user
+    mail(to: @user.email, subject: 'Thanks for auditioning!')
+  end
+
 end
