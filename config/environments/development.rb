d57c04c0cf065735edb6561b5bf9d150c7c4d0d8
Cynthia Wu <ckwu@berkeley.edu>
2019-11-17 21:00:18 -0800

    cover email username and password

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,8 +39,8 @@ Rails.application.configure do
   address:              'smtp.gmail.com',
   port:                 587,
   domain:               'gmail.com',
-  user_name:            'afxroster@gmail.com',
-  password:             'password4afxroster',
+  user_name:            ENV['gmail_username'],
+  password:             ENV['gmail_password'],
   authentication:       'plain',
   enable_starttls_auto: true }
 
