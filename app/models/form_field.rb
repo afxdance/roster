b85c182642da423423276e79b61a3595283a2e9d
Joe Zou <joezou@berkeley.edu>
2020-04-05 18:32:26 -0700

    changes by joe

--- app/models/form_field.rb
+++ app/models/form_field.rb
@@ -1,2 +1,9 @@
 class FormField < ApplicationRecord
+	def self.getTeamSwitchFields
+		return FormField.where(id: 1..16).pluck(:data).map{|data| data.html_safe}
+	end
+
+	def self.setTeamSwitchFields(fields)
+
+	end
 end
