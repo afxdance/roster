0d9df7109d706485c7494933ea75d6867a06cc8d
Evelyn Liu <evelynliu16@berkeley.edu>
2020-04-13 16:50:03 -0700

    Irrelevant files removed

---
+++ app/controllers/dancer_controller.rb
@@ -0,0 +1,12 @@
+class DancerController < ApplicationController
+  def getTeam
+    email = params[:email]
+    puts(email)
+    if (Dancer.exists?(email: email))
+      teams = Dancer.find_by(email: email).teams.map{|team| team.name}
+    else
+      teams = "Dancer not found"
+    end
+    render json: teams.to_json
+  end
+end
