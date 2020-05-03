8da7a5ee139f3204b1a07423e46492b63f9cd414
Michael <mchou359@berkeley.edu>
2020-05-02 21:49:50 -0700

    ??

--- app/controllers/src_controller.rb
+++ app/controllers/src_controller.rb
@@ -10,9 +10,33 @@ class SrcController < ApplicationController
   def submit
     # save form info to db
     # params["name of form element"] such as p_name, p_sign, etc...
-    src = Src.new
+    src = Src.new(src_params)
     # save stuff to src
-    src.save
-    redirect_to "/src/confirm"
+    success = src.save
+    redirect_to "/src/confirm" unless success 
   end
+
+  private
+
+    def src_params
+      params
+        .require(:Src)
+        .permit(
+          :c1,
+          :c2,
+          :c3,
+          :c4,
+          :c5,
+          :c6,
+          :c7,
+          :c8,
+          :c9,
+          :pg_release,
+          :other,
+          :full_name,
+          :signature,
+          :date,
+          :acknowledgement,
+        )
+    end
 end
