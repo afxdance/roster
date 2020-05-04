71d68a3ea247e55eccadb444e01563081ad7715d
Michael <mchou359@berkeley.edu>
2020-05-03 17:01:42 -0700

    oops

--- app/controllers/src_controller.rb
+++ app/controllers/src_controller.rb
@@ -39,27 +39,5 @@ class SrcController < ApplicationController
     redirect_to "/src/confirm" unless success 
   end
 
-  private
-
-    def src_params
-      params
-        .require(:Src)
-        .permit(
-          :c1,
-          :c2,
-          :c3,
-          :c4,
-          :c5,
-          :c6,
-          :c7,
-          :c8,
-          :c9,
-          :pg_release,
-          :other,
-          :full_name,
-          :signature,
-          :date,
-          :acknowledgement,
-        )
-    end
+end
 end
