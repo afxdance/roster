5b2f5013e9f3c3ffae1daadce6c9331b3c730547
Evelyn Liu <evelynliu16@berkeley.edu>
2020-08-16 13:49:07 -0700

    error fixed

--- app/controllers/dancer_controller.rb
+++ app/controllers/dancer_controller.rb
@@ -1,11 +1,11 @@
 class DancerController < ApplicationController
-  def get_team
+  def find_team
     email = params[:email]
     puts(email)
-    if Dancer.exists?(email: email)
-      teams = Dancer.find_by(email: email).teams.map(&:name)
-    else
-      teams = "Dancer not found"
+    teams = if Dancer.exists?(email: email)
+              Dancer.find_by(email: email).teams.map(&:name)
+            else
+              teams = "Dancer not found"
     end
     render json: teams.to_json
   end
--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,7 @@ Rails.application.routes.draw do
   get "/teamswitch", to: "team_switch_form#index"
   post "/teamswitch", to: "team_switch_form#create_team_switch_request", as: "team_switch_form"
 
-  get "/dancer/:email", to: "dancer#get_team",
-  constraints: { email: %r/[^\/]+/}
+  get "/dancer/:email", to: "dancer#find_team",
+                        constraints: {
+                        email: %r/[^\/]+/ }
 end
