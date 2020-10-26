49e93bd25ce8bb915104ab0e05ec1c815e91bffd
Christine Lu <clu@berkeley.edu>
2020-10-25 19:15:24 -0700

    Display error if dancer DNE in DB

--- app/controllers/src_controller.rb
+++ app/controllers/src_controller.rb
@@ -1,5 +1,13 @@
 class SrcController < ApplicationController
   def index
+    @src = Src.new
+    # sees if redirect to index from submit happened
+    if flash[:redirect] == 'true'
+      # adds to errors displayed in index
+      @src.valid?
+    end
+    # resets redirect to false
+    flash[:redirect] = false
     render "index"
   end
 
@@ -12,6 +20,9 @@ class SrcController < ApplicationController
     # params["name of form element"] such as p_name, p_sign, etc...
 
     # TODO, i would reccomend writing all this logic of extracting parameters in the src_params method for cleaner code
+
+    #@src = Src.new(request_params)
+
     @c1 = params["a"]
     @c2 = params["b"]
     @c3 = params["c"]
@@ -31,23 +42,25 @@ class SrcController < ApplicationController
       @signature = params['m_sign']
       @date = params['m_date']
       @acknowledgment = params["m_checkbox"]
-    else 
+    else
       @other = params["pg_other"]
     end
-    dancer = Dancer.where(email: params[:email] )
+
+    dancer = Dancer.where(email: params[:email])
     if dancer.empty?
-      # TODO: return dancer not found error message
-      puts "ERROR DANCER NOT FOUND" # delete this later, i used it for debugging - joe
+      # sets redirect to true when dancer doesn't exist
+      flash[:redirect] = 'true'
+      redirect_to '/src'
     else
-      src = Src.new(c1: @c1, c2: @c2, c3: @c3, c4: @c4, c5: @c5, c6: @c6, c7: @c7, c8: @c8, c9: @c9, pg_release: @pg_release, other: @other, full_name: @full_name, signature: @signature, date: @date, acknowledgment: @acknowledgment)
+      @src = Src.new(c1: @c1, c2: @c2, c3: @c3, c4: @c4, c5: @c5, c6: @c6, c7: @c7, c8: @c8, c9: @c9, pg_release: @pg_release, other: @other, full_name: @full_name, signature: @signature, date: @date, acknowledgment: @acknowledgment)
       # save stuff to src
-      src.dancer = dancer.first
-      success = src.save
+      @src.dancer = dancer.first
+      success = @src.save
       puts success
       # dancer.first.srcs will give a collection of srcs, should only have one but can be multiple bc of has_many
-      redirect_to "/src/confirm" if success 
+      redirect_to "/src/confirm" if success
     end
-    
+
   end
 
   private
@@ -70,7 +83,7 @@ class SrcController < ApplicationController
           :full_name,
           :signature,
           :date,
-          :acknowledgment,
+          :acknowledgment
         )
     end
 end
