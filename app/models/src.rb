e967aec776e2fa6442f1f1a559151f2e4c5c96b2
christineelu <christineluvs997@gmail.com>
2020-11-01 19:56:49 -0800

    Display error if dancer DNE in DB (#85)
    * Display error if dancer DNE in DB
    
    * fix rubocop style errorsin src
    
    * resolve migration issue
    
    * add fixture for srcs
    
    Co-authored-by: Christine Lu <clu@berkeley.edu>

--- app/models/src.rb
+++ app/models/src.rb
@@ -1,4 +1,4 @@
 class Src < ApplicationRecord
-  belongs_to :dancer
-
+  belongs_to :dancer, optional: true
+  validates :dancer, presence: { message: ": You are not in our directory, please revise your information" }
 end
