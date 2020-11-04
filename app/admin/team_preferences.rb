f4d732c7f00c6d70a8b2a7d677a539c651ca635f
Brandon Eng <beng713@gmail.com>
2020-11-03 19:48:48 -0800

    Team preferences admin page

---
+++ app/admin/team_preferences.rb
@@ -0,0 +1,16 @@
+ActiveAdmin.register_page "Team Preferences" do
+  menu label: "Team Preferences", parent: :dancers, if: proc { current_user.admin? }
+
+  page_action :update, method: :post do
+    ActiveRecord::Base.transaction do
+      redirect_to admin_import_dancers_from_csv_path, notice: "Successfully imported dancers"
+    end
+  end
+
+  content do
+    render "admin/team_preferences"
+    # form_for url: { action: "update" } do |f|
+    #   f.submit "Create Teams"
+    # end
+  end
+end
