ee53d1bc204ec9b22dae65a9f5745dd4ea12905f
Brandon Eng <beng713@gmail.com>
2020-11-03 17:16:00 -0800

    working csv upload and database update

---
+++ app/admin/import_preferences.rb
@@ -0,0 +1,44 @@
+ActiveAdmin.register_page "Import Preferences" do
+  menu label: "Import Preferences", parent: :dancers, if: proc { !current_user.admin? }
+
+  # https://activeadmin.info/10-custom-pages.html
+  page_action :update, method: :post do
+    csv = CSV.read(params[:csv][:uploaded_file].tempfile, headers: true, encoding: "bom|utf-8")
+    errors = []
+    ActiveRecord::Base.transaction do
+      dancer_preferences = []
+      director = current_user.id # Gets the currently logged in user
+      teams = Team.all
+      myTeamId = nil
+      for team in teams
+        for user in team.users
+          if user.id == director
+            myTeamId = team.id
+          end
+        end
+      end
+      csv.each do |row|
+        dancer_id = row.to_hash['dancer_id']
+        dancer_preferences.push(dancer_id)
+      end
+      puts dancer_preferences
+
+      pref = TeamPreference.find_by(team_id: myTeamId)
+      puts pref
+      if pref
+        pref.update(team_id: myTeamId, preferences: dancer_preferences)
+      else
+        TeamPreference.create(team_id: myTeamId, preferences: dancer_preferences)
+      end
+      puts TeamPreference.all.inspect
+      redirect_to admin_import_preferences_path, notice: "Successfully imported preferences"
+    end
+  end
+
+  content do
+    form_for :csv, url: { action: "update" }, html: { multipart: true } do |f|
+      f.file_field :uploaded_file
+      f.submit "Submit"
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191118043008) do
+ActiveRecord::Schema.define(version: 20201103170547) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -57,6 +57,14 @@ ActiveRecord::Schema.define(version: 20191118043008) do
 
   create_table "form_fields", force: :cascade do |t|
     t.text "data"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "team_preferences", force: :cascade do |t|
+    t.integer "team_id"
+    t.text "preferences"
+    t.text "initial_team"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
