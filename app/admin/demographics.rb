aa417c0f750e66e9b2054f1c452c40e825e0fa52
Brandon Eng <beng713@gmail.com>
2020-11-05 14:21:33 -0800

    demographics per team

--- app/admin/demographics.rb
+++ app/admin/demographics.rb
@@ -1,7 +1,6 @@
 ActiveAdmin.register_page "Demographics" do
   menu label: "Demographics", parent: :dancers
 
-  menu if: proc { current_user.admin? }
   content do
     render "admin/demographics"
   end
