8af5f635e9f2ccfa22ab3954066c7e897b8c2396
Joe Zou <joezou@berkeley.edu>
2020-11-05 18:39:36 -0800

    Fixed Rubocop version and styling errors

--- app/admin/demographics.rb
+++ app/admin/demographics.rb
@@ -3,20 +3,16 @@ ActiveAdmin.register_page "Demographics" do
 
   content do
     panel "All Dancers" do
-    	render partial: 'admin/demographics', locals: {team_id: nil}
-    	# pie_chart Dancer.group(:gender).count, title: "Gender", width: "300px", height: "300px"
-    	# pie_chart Dancer.group(:year).count, title: "Year", width: "300px", height: "300px"
+      render partial: "admin/demographics", locals: { team_id: nil }
     end
     panel "Teams" do
-    	tabs do
-    		for team in Team.all
-    			tab team.name do
-    				render partial: 'admin/demographics', locals: {team_id: team.id}
-    				# pie_chart Dancer.joins("inner join dancers_teams on dancers.id = dancers_teams.dancer_id").where("dancers_teams.team_id = #{team.id}").group(:gender).count, title: "Gender", width: "300px", height: "300px"
-
-    			end
-    		end
-    	end
+      tabs do
+        for team in Team.all
+          tab team.name do
+            render partial: "admin/demographics", locals: { team_id: team.id }
+          end
+        end
+      end
     end
   end
 end
