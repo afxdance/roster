a5d8b04538c5d7418050ebf40da5668780ac5eb7
Joe Zou <joezou@berkeley.edu>
2020-11-15 14:13:32 -0800

    Fixing rubocop complaints

--- app/admin/demographics.rb
+++ app/admin/demographics.rb
@@ -2,13 +2,13 @@ ActiveAdmin.register_page "Demographics" do
   menu label: "Demographics", parent: :dancers
 
   content do
-    panel "All Dancers (%s)" %[Dancer.count] do
+    panel format("All Dancers (%<count>s)", count: Dancer.count) do
       render partial: "admin/demographics", locals: { team_id: nil }
     end
     panel "Teams" do
       tabs do
         for team in Team.all
-          tab "%s (%s)" %[team.name, Dancer.joins(:dancers_teams).where(dancers_teams: {team_id: team.id}).count] do
+          tab format("%<name>s (%<count>s)", name: team.name, count: Dancer.joins(:dancers_teams).where(dancers_teams: { team_id: team.id }).count) do
             render partial: "admin/demographics", locals: { team_id: team.id }
           end
         end
