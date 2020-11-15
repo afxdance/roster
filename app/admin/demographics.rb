50dbad5d792bd111f056d5b250ad7f7d43cafff5
Joe Zou <joezou@berkeley.edu>
2020-11-15 14:04:16 -0800

    Added num dancers to name of tabs

--- app/admin/demographics.rb
+++ app/admin/demographics.rb
@@ -2,13 +2,13 @@ ActiveAdmin.register_page "Demographics" do
   menu label: "Demographics", parent: :dancers
 
   content do
-    panel "All Dancers" do
+    panel "All Dancers (%s)" %[Dancer.count] do
       render partial: "admin/demographics", locals: { team_id: nil }
     end
     panel "Teams" do
       tabs do
         for team in Team.all
-          tab team.name do
+          tab "%s (%s)" %[team.name, Dancer.joins(:dancers_teams).where(dancers_teams: {team_id: team.id}).count] do
             render partial: "admin/demographics", locals: { team_id: team.id }
           end
         end
