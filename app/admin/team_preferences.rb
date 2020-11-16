c0da83910adf33189cf0b37d968b109cad28ed1b
Brandon Eng <beng713@gmail.com>
2020-11-16 14:25:06 -0500

    setting initial teams in team_preferences table and clearing it

--- app/admin/team_preferences.rb
+++ app/admin/team_preferences.rb
@@ -8,12 +8,25 @@ ActiveAdmin.register_page "Team Preferences" do
     if !result[:errors].empty?
       redirect_to admin_team_preferences_path(errors: result[:errors])
     else
+
+      preferences = TeamPreference.all
+      for pref in preferences
+        pref.update(initial_team: result[:teams][pref.team_id])
+      end
+
       redirect_to admin_team_preferences_path(teams: result[:teams], limbo: result[:limbo])
     end
   end
 
   page_action :delete_teams, method: :post do
+    # clears our dancer_teams table
     ActiveRecord::Base.connection.execute("DELETE from 'dancers_teams'")
+    # clears our "initial_team" column in team_preferences table
+    preferences = TeamPreference.all
+    for pref in preferences
+      pref.update(initial_team: nil)
+    end
+
     redirect_to admin_team_preferences_path(message: "All teams have been cleared")
   end
 
