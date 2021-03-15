4ef8e80fd73b17ba9793cb2b5184a3d4bf565c4f
Dustin Luong <dstn.luong@berkeley.edu>
2021-03-14 23:05:37 -0700

    Added comment explaining convoluted line

--- app/models/team_switch_request.rb
+++ app/models/team_switch_request.rb
@@ -21,8 +21,10 @@ class TeamSwitchRequest < ApplicationRecord
     :new_team,
   ].freeze
 
+  # Return relevant team switch requests, either the dancer just joined or just left
+  # If the new team isn't nil then that means the request has been processed
   def self.get_processed_team_switch_requests(team_id)
     TeamSwitchRequest.where.not(new_team_id: nil).where(new_team_id: team_id)
-      .or(TeamSwitchRequest.where.not(new_team_id: nil).where(old_team_id: team_id))
+                     .or(TeamSwitchRequest.where.not(new_team_id: nil).where(old_team_id: team_id))
   end
 end
