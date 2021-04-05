ed516cdb8a93a6525e8fde057e833ac4e2cc25f5
Michael Ren <michael_ren@berkeley.edu>
2021-04-04 20:58:37 -0700

    Fixed annoying css bug

--- app/models/src_form_field.rb
+++ app/models/src_form_field.rb
@@ -2,22 +2,12 @@ class SrcFormField < ApplicationRecord
   SRC_RANGE = (1..4)
   SRC_BACKUP_RANGE = (4..8)
 
-
   def self.find_src_form_fields
-    return SrcFormField.src.pluck(:data).map(&:html_safe)
+    return SrcFormField.src.where.not(data: nil).pluck(:data).map(&:html_safe)
   end
 
   def self.update_src_form_fields(fields)
-    # puts "src_form_field start"
-    # puts fields
-    # puts SrcFormField.src
-    # puts "src_form_field end"
 
-    # if SrcFormField.src.size < SRC_RANGE.size
-    #   for i in SRC_RANGE
-    #     SrcFormField.create(id: i, data: "testing", created_at: "Sun, 14 Mar 2021 20:16:10 PDT -07:00", updated_at: "Sun, 14 Mar 2021 20:16:10 PDT -07:00")
-    #   end
-    # end
     SrcFormField.src.each_with_index do |item, index|
       # puts "item, index"
       # puts item
