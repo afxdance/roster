58b39964fa83a48563248703a8956546a81c65c6
Evelyn Liu <evelynliu16@berkeley.edu>
2021-06-05 16:33:39 +0800

    change production.rb and upgrade gems

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,7 +76,8 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
 
-
+  config.serve_static_assets = true
+  config.assets.compile = true
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
