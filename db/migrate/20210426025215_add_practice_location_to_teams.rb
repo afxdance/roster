b2278d6675f2774b3ec636eead22f69b2bc950f2
Joe Zou <joezou@berkeley.edu>
2021-06-29 20:45:03 -0500

    Changed practice location to regular string from array, broke some migration rules but issok

--- db/migrate/20210426025215_add_practice_location_to_teams.rb
+++ db/migrate/20210426025215_add_practice_location_to_teams.rb
@@ -1,5 +1,5 @@
 class AddPracticeLocationToTeams < ActiveRecord::Migration[5.1]
   def change
-    add_column :teams, :practice_location, :string, array: true
+    add_column :teams, :practice_location, :string
   end
 end
--- db/migrate/20210605030402_change_user_director_column_name.rb
+++
@@ -1,5 +0,0 @@
-class ChangeUserDirectorColumnName < ActiveRecord::Migration[5.1]
-  def change
-    rename_column :users, :director_name, :name
-  end
-end
