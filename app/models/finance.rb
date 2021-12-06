ad24c86fce97006d21c1d29829cf667b260ced27
Junna Chen <junnachen2001@gmail.com>
2021-12-05 19:13:33 -0800

    fixing rubocop errors

--- app/models/finance.rb
+++ app/models/finance.rb
@@ -2,11 +2,7 @@ class Finance < ActiveRecord::Base
   belongs_to :dancer
   before_save :default_values
   def default_values
-    if self.tickets.nil?
-      self.tickets = false
-    end
-    if self.dues.nil?
-      self.dues = false
-    end
+    self.tickets = false if tickets.nil?
+    self.dues = false if dues.nil?
   end
 end
