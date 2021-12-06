b2e7f8cd623d5b25b61be9bb1127a23029a020e1
Junna Chen <junnachen2001@gmail.com>
2021-12-05 19:03:13 -0800

    more default value work for tickets and dues

--- app/models/finance.rb
+++ app/models/finance.rb
@@ -1,3 +1,12 @@
 class Finance < ActiveRecord::Base
   belongs_to :dancer
+  before_save :default_values
+  def default_values
+    if self.tickets.nil?
+      self.tickets = False
+    end
+    if self.dues.nil?
+      self.dues = False
+    end
+  end
 end
