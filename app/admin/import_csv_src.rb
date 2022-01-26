343bc4f0170fe2b28e838d9b57b8b22ad7711562
Evelyn Liu <evelynliu16@berkeley.edu>
2022-01-26 15:35:50 -0800

    fix conflicts

--- app/admin/import_csv_src.rb
+++ app/admin/import_csv_src.rb
@@ -5,7 +5,7 @@ require "fileutils"
 
 # https://gist.github.com/peterclark/df7e68e62cb97385beb46685c66a8606
 # https://github.com/googleworkspace/ruby-samples/blob/master/sheets/quickstart/quickstart.rb
-scopes =  ['https://www.googleapis.com/auth/spreadsheets']
+scopes = ["https://www.googleapis.com/auth/spreadsheets"]
 authorization = Google::Auth.get_application_default(scopes)
 
 auth_client = authorization.dup
@@ -14,7 +14,21 @@ service = Google::Apis::SheetsV4::SheetsService.new
 service.authorization = auth_client
 
 ActiveAdmin.register_page "Import SRC from CSV" do
+  menu if: proc { current_user.admin? }
   menu label: "Import SRC from CSV", parent: :dancers
+
+  content do
+    form_for :csv, url: { action: "update" }, html: { multipart: true } do |f|
+      f.file_field :uploaded_file
+      f.submit "Submit CSV"
+    end
+
+    # create a form field
+    form_for :urls, url: { action: "submiturl" } do |f|
+      f.url_field :google_form_url, placeholder: "Google Sheets URL"
+      f.submit "Submit Google Sheets URL"
+    end
+  end
 
   # https://activeadmin.info/10-custom-pages.html
   page_action :update, method: :post do
@@ -23,17 +37,17 @@ ActiveAdmin.register_page "Import SRC from CSV" do
     ActiveRecord::Base.transaction do
       csv.each do |row|
         email = row[0]
-        for dancer in Dancer.all  #@dancers = Dancer.all
+        Dancer.all.each do |dancer|
           if email == dancer.email
             dancer.update(src_submitted: true)
           end
         end
-      end
-      if !errors.empty?
-        redirect_to admin_import_src_from_csv_path, notice: "There are #{errors.length} invalid src : \n #{errors}"
-        raise ActiveRecord::Rollback
-      else
-        redirect_to admin_import_src_from_csv_path, notice: "Successfully imported src csv"
+        if !errors.empty?
+          redirect_to admin_import_src_from_csv_path, notice: "There are #{errors.length} invalid src : \n #{errors}"
+          raise ActiveRecord::Rollback
+        else
+          redirect_to admin_import_src_from_csv_path, notice: "Successfully imported src csv"
+        end
       end
     end
   end
@@ -47,28 +61,13 @@ ActiveAdmin.register_page "Import SRC from CSV" do
 
     ActiveRecord::Base.transaction do
       response.values.each do |row|
-        for dancer in Dancer.all
+        Dancer.all.each do | dancer |
           if dancer.email == row[0]
             dancer.update(src_submitted: true)
           end
         end
       end
-
       redirect_to admin_import_src_from_csv_path, notice: "Successfully updated from Google Sheets"
-    end
-  end
-
-  content do
-    form_for :csv, url: { action: "update" }, html: { multipart: true } do |f|
-      f.file_field :uploaded_file
-      f.submit "Submit CSV"
-    end
-
-    #create a form field
-    form_for :urls, url: { action: "submiturl" } do |f|
-
-      f.url_field :google_form_url, placeholder: "Google Sheets URL"
-      f.submit "Submit Google Sheets URL"
     end
   end
 end
