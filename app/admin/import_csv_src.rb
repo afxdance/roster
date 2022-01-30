08881c6ff43fd792662be981ff2adfd0ace05a7a
Dustin Luong <dstn.luong@berkeley.edu>
2022-01-29 22:40:01 -0800

    don't worry about authorization until we actually use the import function (warning: hides problem until later if authorization isn't setup correctly)

--- app/admin/import_csv_src.rb
+++ app/admin/import_csv_src.rb
@@ -3,16 +3,6 @@ require "googleauth"
 require "googleauth/stores/file_token_store"
 require "fileutils"
 
-# https://gist.github.com/peterclark/df7e68e62cb97385beb46685c66a8606
-# https://github.com/googleworkspace/ruby-samples/blob/master/sheets/quickstart/quickstart.rb
-scopes = ["https://www.googleapis.com/auth/spreadsheets"]
-authorization = Google::Auth.get_application_default(scopes)
-
-auth_client = authorization.dup
-auth_client.fetch_access_token!
-service = Google::Apis::SheetsV4::SheetsService.new
-service.authorization = auth_client
-
 ActiveAdmin.register_page "Import SRC from CSV" do
   menu if: proc { current_user.admin? }
   menu label: "Import SRC from CSV", parent: :dancers
@@ -53,6 +43,15 @@ ActiveAdmin.register_page "Import SRC from CSV" do
   end
 
   page_action :submiturl, method: :post do
+    # https://gist.github.com/peterclark/df7e68e62cb97385beb46685c66a8606
+    # https://github.com/googleworkspace/ruby-samples/blob/master/sheets/quickstart/quickstart.rb
+    scope = "https://www.googleapis.com/auth/spreadsheets"
+    authorization = Google::Auth.get_application_default(scope)
+
+    auth_client = authorization.dup
+    auth_client.fetch_access_token!
+    service = Google::Apis::SheetsV4::SheetsService.new
+    service.authorization = auth_client
     url_link = params[:urls][:google_form_url]
     spreadsheet_id = url_link.split(%r{\/(?=[\w])})[-2]
 
