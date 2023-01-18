4203660060c8b3227e8915304b1f63601ed612b0
charisseliu <113862165+charisseliu@users.noreply.github.com>
2023-01-17 16:37:51 -0800

    Update redis.rb

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -3,8 +3,8 @@
 # TODO: on prod, we need to use Heroku instead of a local Redis server. So `REDIS` below will
 # need to be defined differently based on whether we're on dev or prod.
 
-require "redis"
-REDIS = Redis.new
+uri = URI.parse(ENV[postgres://fnfgluscuuumbu:72e86abee74de58b6c9355d4060766d937e58cd77f9153639033df8f4d1d9550@ec2-52-3-2-245.compute-1.amazonaws.com:5432/davmj8ci67fa5] || "redis://localhost:6379/" )
+REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
 
 # REDIS.set("camp_interest", true)
 # REDIS.set("exp_interest", false)
